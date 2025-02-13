4bdf3a2b4121b96688a82c4c006fa44693ca59bd
keval-rahul <k17r16patel@gmail.com>
2025-02-13 15:15:26 +0530

    test update work or not

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,4 +3,6 @@
 <h2>Welcome to the Home Page!</h2>
 <p>This is where you can introduce your website or application to users.</p>
 
+<h1>Hello keval all work </h1>
+
 <?php include '../app/views/includes/footer.php'; ?>
