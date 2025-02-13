441f503baab96053310f6008cfd167d22c4528c4
keval-rahul <k17r16patel@gmail.com>
2025-02-13 15:09:30 +0530

    update port

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN docker-php-ext-install mysqli pdo pdo_mysql
 WORKDIR /var/www/html
 COPY . /var/www/html/
 RUN sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/public|g" /etc/apache2/sites-available/000-default.conf
-EXPOSE 8081
+EXPOSE 80
 CMD ["apache2-foreground"]
--- public/index.php
+++ public/index.php
@@ -5,17 +5,31 @@ require_once '../app/core/Database.php';
 require_once '../app/controllers/HomeController.php';
 
 // Routing Logic
-$url = isset($_GET['url']) ? explode('/', filter_var(trim($_GET['url'], '/'), FILTER_SANITIZE_URL)) : ['HomeController', 'index'];
+$url = isset($_GET['url']) ? explode('/', filter_var(trim($_GET['url'], '/'), FILTER_SANITIZE_URL)) : ['Home', 'index'];
 
-$controller = !empty($url[0]) ? ucfirst($url[0]) . 'Controller' : 'HomeController';
+$controllerName = ucfirst($url[0]);
+$controllerFile = "../app/controllers/{$controllerName}Controller.php";
+
+// Check if the controller file exists before requiring it
+if (!file_exists($controllerFile)) {
+    die("Controller file not found: " . $controllerFile);
+}
+
+require_once $controllerFile;
+$controllerClass = $controllerName . 'Controller';
+
+if (!class_exists($controllerClass)) {
+    die("Controller class not found: " . $controllerClass);
+}
+
+$instance = new $controllerClass();
 $method = isset($url[1]) ? $url[1] : 'index';
 
-require_once "../app/controllers/$controller.php";
-$instance = new $controller();
-
+// Check if the method exists in the controller
 if (method_exists($instance, $method)) {
     call_user_func_array([$instance, $method], array_slice($url, 2));
 } else {
-    die("Method not found");
+    die("Method '$method' not found in $controllerClass");
 }
 ?>
+
